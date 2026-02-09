59d86db4113ee0623f1c2a80495afc459c021568
Odili <odiliejeh09@gmail.com>
2026-02-09 01:02:42 +0100

    "Projects"

--- src/Stack/ListPractice.java
+++ src/Stack/ListPractice.java
@@ -1,7 +1,16 @@
 package Stack;
 
 public class ListPractice {
+
+    private int index = -1;
+    private String[] list = new String[4];
+
     public boolean isEmpty() {
-        return false;
+        return index == - 1;
+    }
+
+    public void add(String input) {
+        list[index += 1] = input;
+
     }
 }
--- test/Stack/ListTest.java
+++ test/Stack/ListTest.java
@@ -1,15 +1,29 @@
 package Stack;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ListTest {
+    ListPractice list;
+
+    @BeforeEach
+    public void startEachMethodWith(){
+        list = new ListPractice();
+    }
 
     @Test
     public void testThatListIsEmpty(){
-        ListPractice list = new ListPractice();
         assertTrue(list.isEmpty());
+    }
+
+    @Test
+    public void testThatICanAddElementsToListAndSizeIncreases(){
+        list.add("Ada");
+        list.add("Obinna");
+//        assertEquals(2, list.getSize());
     }
 
 }
