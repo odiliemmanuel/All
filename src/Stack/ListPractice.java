a8083c2fdd451c7b636c37ac541eb9f404e88ae5
Odili <odiliejeh09@gmail.com>
2026-02-09 06:37:56 +0100

    "Projects"

--- src/Stack/ListPractice.java
+++ src/Stack/ListPractice.java
@@ -2,15 +2,65 @@ package Stack;
 
 public class ListPractice {
 
+    private int capacity = 4;
+    private String[] list = new String[capacity];
     private int index = -1;
-    private String[] list = new String[4];
+    private int  size = 0;
+
 
     public boolean isEmpty() {
+
         return index == - 1;
     }
 
+
     public void add(String input) {
-        list[index += 1] = input;
+        if(size == list.length) {
+            capacity *= capacity;
+
+            list[index += 1] = input;
+
+        }
+        size++;
+    }
+
+    public void addTo(int position, String input){
+        if(size == list.length){
+            capacity *= capacity;
+
+            list[position] = input;
+        }
+        size ++;
+    }
+
+
+    public int size(){
+
+        return size;
+    }
+
+
+    public String remove(String input) {
+        if(isEmpty()){
+           throw new IllegalArgumentException("Operation cannot work for Empty list");
+
+        }
+
+        for(int count = 0; count < size; count ++) {
+            String result = "";
+            if (list[count].equals(input)){
+                result = list[count];
+            }
+
+            size -= 1;
+            return result;
+        }
+
+        return null;
 
     }
+
+//    public String removeFrom(int indexInput) {
+//
+//    }
 }
--- test/Stack/ListTest.java
+++ test/Stack/ListTest.java
@@ -23,7 +23,60 @@ public class ListTest {
     public void testThatICanAddElementsToListAndSizeIncreases(){
         list.add("Ada");
         list.add("Obinna");
-//        assertEquals(2, list.getSize());
+        assertEquals(2, list.size());
+    }
+
+    @Test
+    public void testThatICanAddANewElementViaSpecificationOfIndex(){
+        assertTrue(list.isEmpty());
+        list.addTo(1,"Ada");
+        list.add("Obinna");
+        assertEquals(2, list.size());
+    }
+
+    @Test
+    public void testThatWhenIRemoveAnElement_SizeReduces(){
+        assertTrue(list.isEmpty());
+        list.addTo(1,"Ada");
+        list.add("Obinna");
+        list.addTo(0, "chibuzor");
+        assertEquals(3, list.size());
+
+        list.remove("Ada");
+        assertEquals(2, list.size());
+    }
+
+    @Test
+    public void testThatIfInputIsEmptyErrorIsThrown(){
+        assertTrue(list.isEmpty());
+        list.addTo(1,"Ada");
+        list.add("Obinna");
+        list.addTo(0, "chibuzor");
+        assertEquals(3, list.size());
+
+        list.remove("");
+        assertEquals(2, list.size());
+    }
+
+    @Test
+    public void testThatIfIAddAndReachMaximumLength_LengthIncreases(){
+        list.add("obi");
+        list.add("ada");
+        list.add("obi");
+        list.add("ada");
+        list.add("obi");
+        list.add("ada");
+        assertEquals(6, list.size());
+    }
+
+    @Test
+    public void tesThatICanRemoveElementViaIndex(){
+        list.add("obi");
+        list.add("ada");
+        list.add("obinna");
+
+//        list.removeFrom(1);
+        assertEquals(2, list.size());
     }
 
 }
